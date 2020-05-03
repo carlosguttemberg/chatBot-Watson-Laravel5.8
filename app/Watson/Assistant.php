c2cfda1f65eb49e87c10c4ec7fd23722d58ed0f0
carlosguttemberg <guttembergcarlos@gmail.com>
2020-05-03 12:40:10 -0300

    Primeiro Commit

---
+++ app/Watson/Assistant.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Watson;
+
+use GuzzleHttp\Client;
+
+class Assistant
+
+{
+    private $username;
+    private $password;
+    private $workspace;
+
+
+    public function __construct(string $username, string $password, string $workspace){
+        $this->username = $username; 
+        $this->password = $password; 
+        $this->workspace = $workspace; 
+    }
+
+
+    public function dialog(string $message){
+        $client = new Client;
+
+        $url = 'http://gateway.watsonplatform.net/assistant/api/v1/workspaces/' . $this->workspace . '/message?version=2018-09-20';
+        $response = $client->request('POST', $url, [
+            'json' => [
+                'input' => [
+                    'text' => $message
+                ]
+            ],
+            'auth' => [
+                $this->username, $this->password
+            ]
+        ]);
+
+        return (string) $response->getBody();
+    }
+}
+
+?>
